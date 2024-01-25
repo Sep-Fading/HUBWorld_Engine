00539e7d74379129faed1de61556cf841f3f7a1d
Sepehr <main.sepehr@gmail.com>
2024-01-25 17:46:15 +0000

    cmake setup

---
+++ cge_source/CMakeFiles/cge_macos_build.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cge_macos_build.dir/main.cpp.o"
+  "CMakeFiles/cge_macos_build.dir/main.cpp.o.d"
+  "debug/cge_macos_build"
+  "debug/cge_macos_build.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cge_macos_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cge_source/CMakeFiles/cge_macos_build.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cge_macos_build.
+# This may be replaced when dependencies are built.
